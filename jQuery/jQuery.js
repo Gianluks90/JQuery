953b8587c4103c27b0ed1a5e692f532cfb5df8c8
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2019-02-07 08:38:10 +0100

    Initial commit;

---
+++ jQuery/jQuery.js
@@ -0,0 +1,58 @@
+let visibleMethods = (function ($) {
+
+    const SOURCE = "https://jsonplaceholder.typicode.com/users/";
+
+    function LoadDoc(source, displayFunction) {
+        $.get(source, function(data, status) {
+            if(status == "success") {
+                displayFunction(data);
+            }
+        })
+    };
+
+    function DisplayLink(users) {
+        for (let i = 0; i < users.length; i++) {
+            let user = users[i];
+            let div = document.getElementById("Info");
+            let paragraph = document.createElement("p");
+            paragraph.setAttribute("data-paragraphId", user.id);
+            let link = document.createElement("a");
+            link.setAttribute("href", "");
+            link.innerHTML = user.name;
+            link.setAttribute("data-userId", user.id);
+            link.appendChild(document.createElement('br'));
+            paragraph.appendChild(link);
+            div.appendChild(paragraph);
+            div.addEventListener("click", LoadUser) // ritorna anche un event (evt) nascosto;
+        }
+    }
+
+    function LoadUser(evt){
+        let userId = evt.target.getAttribute("data-userId");
+        let newSource = SOURCE + userId;
+        LoadDoc(newSource, DisplayDetails);
+        evt.preventDefault();
+    }
+
+    function DisplayDetails(user){
+        let userId = user.id;
+        let paragraph = document.querySelector("[data-paragraphId = '"+ userId +"']");
+        let span1 = document.createElement('span');
+        span1.innerHTML = "e-mail: " + user.email + "<br>City: " + user.address.city;
+        span1.appendChild(document.createElement('br'));
+        paragraph.appendChild(span1);
+    }
+
+    function FirstLoad(){
+        let div = document.getElementById("Info");
+        $(div).empty();
+        LoadDoc(SOURCE, DisplayLink);
+    }
+
+    return {
+        firstLoad : FirstLoad
+    }
+})($);
+
+let button = document.getElementById("getButton");
+button.onclick = visibleMethods.firstLoad;
